3ca7a3e48714fb750174a3be8a32899868690a21
Mark <markhuna@gmail.com>
2023-07-04 03:34:30 +0400

    u

---
+++ Remove-Temp-Files.ps1
@@ -0,0 +1,4 @@
+Remove-Item -Path "$env:userprofile\AppData\Local\Temp\*" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "C:\Windows\Temp\*" -Recurse -Force -ErrorAction SilentlyContinue
+Remove-Item -Path "C:\Windows\Prefetch\*" -Recurse -Force -ErrorAction SilentlyContinue
+Clear-RecycleBin -Force
